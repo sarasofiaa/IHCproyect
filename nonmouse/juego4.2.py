456100f94fa37d96d0c784e1dc2233a3871d85a1
DeniseHuacani <dhuacanij@unsa.edu.pe>
2024-12-08 22:42:16 -0500

    Secambia el metodo para que se distinga entre insectos y mascotas y se le asigna el tag de acuerdo a que tipo es

--- nonmouse/juego4.2.py
+++ nonmouse/juego4.2.py
@@ -10,8 +10,9 @@ error = 0  # ¿Para qué se usaría este? Puedes aclararlo si lo necesitas
 tiempo = 20000  # Tiempo de juego en milisegundos (20 segundos)
 posiciones_y = [80, 180, 280, 380, 480]  
 
-def crear_gif_con_fondo(root, gif_rutas, fondo_ruta, width, height, gif_height):  
-    frames_resized_all_gifs = []
+def crear_gif_con_fondo(root, insectos_rutas, mascotas_rutas, fondo_ruta, width, height, gif_height):  
+    frames_resized_all_insectos = []
+    frames_resized_all_mascotas = []
 
     # Crear el canvas con las dimensiones de la imagen
     canvas = Canvas(root, width=width, height=height)
@@ -25,10 +26,11 @@ def crear_gif_con_fondo(root, gif_rutas, fondo_ruta, width, height, gif_height):
     
     canvas.create_image(0, 0, image=fondo_tk, anchor=NW)  # Coloca la imagen de fondo
 
-    for gif_ruta in gif_rutas:
+    # Cargar los GIFs de insectos
+    for gif_ruta in insectos_rutas:
         gif_image = Image.open(gif_ruta)  # Cargar el GIF
         framesNum = gif_image.n_frames  # Número de frames que tiene el gif
-        print(framesNum)
+        print(f"Frames de insecto: {framesNum}")
 
         gif_original_width, gif_original_height = gif_image.size
 
@@ -47,26 +49,59 @@ def crear_gif_con_fondo(root, gif_rutas, fondo_ruta, width, height, gif_height):
             frame_image = frame.subsample(int(frame.width() // gif_width), int(frame.height() // gif_height))
             frames_resized.append(frame_image)
         
-        # Almacenar los frames redimensionados de cada gif
-        frames_resized_all_gifs.append(frames_resized)
+        # Almacenar los frames redimensionados de cada gif de insectos
+        frames_resized_all_insectos.append(frames_resized)
+
+    # Cargar los GIFs de mascotas
+    for gif_ruta in mascotas_rutas:
+        gif_image = Image.open(gif_ruta)  # Cargar el GIF
+        framesNum = gif_image.n_frames  # Número de frames que tiene el gif
+        print(f"Frames de mascota: {framesNum}")
+
+        gif_original_width, gif_original_height = gif_image.size
+
+        # Calcular el nuevo ancho basado en la altura proporcionada (manteniendo la proporción original)
+        aspect_ratio = gif_original_width / gif_original_height
+        gif_width = int(aspect_ratio * gif_height)
+
+        # Lista de todas las imágenes del gif
+        frames = [
+            PhotoImage(file=gif_ruta, format='gif -index %i' % (i)) for i in range(framesNum)
+        ]
+        frames_resized = []
+    
+        # Redimensionar los frames del gif según los nuevos tamaños proporcionados
+        for frame in frames:
+            frame_image = frame.subsample(int(frame.width() // gif_width), int(frame.height() // gif_height))
+            frames_resized.append(frame_image)
+        
+        # Almacenar los frames redimensionados de cada gif de mascotas
+        frames_resized_all_mascotas.append(frames_resized)
 
     # Diccionario para almacenar el ID  de cada GIF
     gif_ids = {}
 
     # Función para generar nuevos GIFs aleatorios
     def generar_gif_aleatorio():
-        print("Se genero un gif aleatorio")
-        # Elegir un GIF aleatorio
-        gif_ruta = random.choice(gif_rutas)
-        frames_resized = frames_resized_all_gifs[gif_rutas.index(gif_ruta)]
+        print("Se generó un gif aleatorio")
+        # Elegir un GIF aleatorio de insectos o mascotas
+        grupo = random.choice(['insecto', 'mascota'])
+        if grupo == 'insecto':
+            gif_ruta = random.choice(insectos_rutas)
+            frames_resized = frames_resized_all_insectos[insectos_rutas.index(gif_ruta)]
+            tag = f"insecto_{random.randint(1000, 9999)}"  # Genera un tag único para cada gif de insecto
+        else:
+            gif_ruta = random.choice(mascotas_rutas)
+            frames_resized = frames_resized_all_mascotas[mascotas_rutas.index(gif_ruta)]
+            tag = f"mascota_{random.randint(1000, 9999)}"  # Genera un tag único para cada gif de mascota
+        
         pos_x = width
         pos_y = random.choice(posiciones_y)
-        tag = f"gif_{random.randint(1000, 9999)}"  # Genera un tag único para cada gif
         # Iniciar el ciclo de actualización de este gif
-        gif_ids[tag] = root.after(0, update_gif, 0, frames_resized, tag, pos_x, pos_y)
+        gif_ids[tag] = root.after(0, update_gif, 0, frames_resized, tag, pos_x, pos_y, grupo)
 
     # Actualizar la imagen del gif en el canvas
-    def update_gif(ind, frames_resized, tag, pos_x, pos_y):
+    def update_gif(ind, frames_resized, tag, pos_x, pos_y, grupo):
         """Actualiza la imagen gif."""
         global insects_pass, pet_pass, tiempo
 
@@ -87,15 +122,15 @@ def crear_gif_con_fondo(root, gif_rutas, fondo_ruta, width, height, gif_height):
         posDesaparece = 120
         if pos_x < posDesaparece:
             pos_x = width
-            if 'insecto' in tag:
+            if grupo == 'insecto':
                 insects_pass += 1  # Incrementa si es un insecto
+                fin_del_juego()
             else:
                 pet_pass += 1  # Incrementa si es una mascota
             print(f"Insectos pasados: {insects_pass}, Mascotas pasadas: {pet_pass}")
-            print(tag)
 
         # Reprograma la actualización del GIF
-        gif_ids[tag] = root.after(100, update_gif, ind, frames_resized, tag, pos_x, pos_y)  # Número que regula la velocidad del gif
+        gif_ids[tag] = root.after(100, update_gif, ind, frames_resized, tag, pos_x, pos_y, grupo)  # Número que regula la velocidad del gif
 
     # Generar GIFs repetidamente
     def generar_gifs_repetidamente():
@@ -108,29 +143,28 @@ def crear_gif_con_fondo(root, gif_rutas, fondo_ruta, width, height, gif_height):
             fin_del_juego()
 
     def fin_del_juego():
-        print("Fin del juego")
+        global insects_pass
+        if insects_pass == 0:  # Si no se ha dejado pasar ningún insecto
+            print("¡Has ganado! Tiempo terminado y no ha pasado ningún insecto.")
+        else:
+            print(f"Game Over! Insectos pasados: {insects_pass}, Mascotas pasadas: {pet_pass}")
 
     # Función para eliminar el GIF al hacer clic
     def eliminar_gif(event):
         # Obtener las coordenadas del clic
         x, y = event.x, event.y
         entidad_id = canvas.find_closest(x, y)[0]  # Obtén el ID del objeto más cercano
-        print(f"Entidad ID: {entidad_id}")
         tags = canvas.gettags(entidad_id)  # Obtén las tags asociadas al objeto
-        print(f"Tags: {tags}")
 
         if tags:
             tag = tags[0]  # Tomamos el primer tag
             print(f"Tag del objeto: {tag}")
-            # Eliminamos el GIF si existe
+            # Eliminar el GIF
             if tag in gif_ids:
                 root.after_cancel(gif_ids[tag])  # Detener la actualización
                 del gif_ids[tag]  # Eliminar el 'after' del diccionario
                 canvas.delete(tag)
                 print(f"GIF con tag {tag} eliminado.")
-                
-            else:
-                print(f"Tag {tag} no encontrado en gifs_tags.")
 
     # Vincular el evento de clic en el canvas
     canvas.bind("<Button-1>", eliminar_gif)
@@ -141,11 +175,10 @@ def crear_gif_con_fondo(root, gif_rutas, fondo_ruta, width, height, gif_height):
 
 def logicaJuego4(): 
     # Fondo en canvas
-    base_dir = os.path.dirname(os.path.abspath(__file__)) # Obtiene la dirección actual
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # Obtiene la dirección actual
     ruta_fondo = os.path.join(base_dir, "..", "images", "juego4", "fondoPatio.png")
 
     # Carga de gifs
-    # Rutas
     ruta_insecto1 = os.path.join(base_dir, "..", "images", "juego4", "insecto1.gif")
     ruta_mascota1 = os.path.join(base_dir, "..", "images", "juego4", "perro2.gif")
 
@@ -157,7 +190,8 @@ def logicaJuego4():
     ruta_imagen_fondo_normalizada = os.path.normpath(ruta_fondo)
 
     crear_gif_con_fondo(root, 
-                        [ruta_gif_insecto1_normalizada, ruta_gif_mascota1_normalizada], 
+                        [ruta_gif_insecto1_normalizada],  # Lista de insectos
+                        [ruta_gif_mascota1_normalizada],  # Lista de mascotas
                         ruta_imagen_fondo_normalizada, 
                         width=1100, height=600, gif_height=100)
 
